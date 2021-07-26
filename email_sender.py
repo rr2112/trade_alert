bda25f183e916d93aebbf0aebfc6c37695da359a
Rakesh <d.rakeshreddy2@gmail.com>
2021-07-26 16:16:32 +0530

    creds update

--- email_sender.py
+++ email_sender.py
@@ -1,23 +1,20 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import os
 
 def send_email(mail_content):
-    #The mail addresses and password
-    sender_address = 'cryptosignalalert2112@gmail.com'
-    sender_pass = 'Talib@123#'
-    receiver_address = 'cryptosignalalert2112@gmail.com'
-    #Setup the MIME
+    sender_address = os.environ.get('email_sender_user')
+    sender_pass = os.environ.get('email_sender_password')
+    receiver_address = os.environ.get('email_sender_user')
     message = MIMEMultipart()
     message['From'] = sender_address
     message['To'] = receiver_address
-    message['Subject'] = 'Crypto Alerts'   #The subject line
-    #The body and the attachments for the mail
+    message['Subject'] = 'Crypto Alerts'  
     message.attach(MIMEText(mail_content, 'plain'))
-    #Create SMTP session for sending the mail
-    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-    session.starttls() #enable security
-    session.login(sender_address, sender_pass) #login with mail_id and password
+    session = smtplib.SMTP('smtp.gmail.com', 587) 
+    session.starttls() 
+    session.login(sender_address, sender_pass)
     text = message.as_string()
     session.sendmail(sender_address, receiver_address, text)
     session.quit()
